fb33c9d99140a2b11ab101f4a1d85c75df18529d
LeticiadelaOsa <deletidev@gmail.com>
2023-07-29 13:59:35 +0200

    img

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,24 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import "../scss/styles.scss";
+import '../scss/styles.scss';
+import close from '../assets/images/icon-close.svg';
+import menuImg from '../assets/images/icon-hamburger.svg';
+const menu = document.getElementById('menu');
+const header = document.getElementById('header');
+const botonMenu = document.getElementById('boton-menu');
 
-const menu = document.getElementById("menu");
-const header = document.getElementById("header");
-const botonMenu = document.getElementById("boton-menu");
-
-botonMenu.addEventListener("click", () => {
-  menu.classList.toggle("menu--open");
-  if (menu.classList.contains("menu--open")) {
-    botonMenu.src = "assets/images/icon-close.svg";
+botonMenu.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+  if (menu.classList.contains('menu--open')) {
+    botonMenu.src = close;
   } else {
-    botonMenu.src = "assets/images/icon-hamburger.svg";
+    botonMenu.src = menuImg;
   }
 });
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY >= 50) {
-    header.classList.add("header--background");
+    header.classList.add('header--background');
   } else {
-    header.classList.remove("header--background");
+    header.classList.remove('header--background');
   }
-  menu.classList.remove("open");
+  menu.classList.remove('open');
 });
